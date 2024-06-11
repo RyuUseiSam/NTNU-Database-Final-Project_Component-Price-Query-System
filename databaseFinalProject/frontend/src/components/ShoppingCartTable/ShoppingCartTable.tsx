92d67083c1326adae206deced2c0d52e31e8f2b9
Andrea Xu <108392545+ABCDEFG123ddre@users.noreply.github.com>
2024-06-12 01:33:24 +0800

    Update ShoppingCartTable.tsx

--- databaseFinalProject/frontend/src/components/ShoppingCartTable/ShoppingCartTable.tsx
+++ databaseFinalProject/frontend/src/components/ShoppingCartTable/ShoppingCartTable.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { MdDeleteForever } from "react-icons/md";
-
-import ramImg from '../../assets/images/ram.png';
-import hddImg from '../../assets/images/hdd.png';
-import ssdImg from '../../assets/images/ssd.png';
 import './ShoppingCartTable.scss';
+
+const getData = async () => {
+    let response = await fetch("http://127.0.0.1:8000/api/getPurchaseList/");
+    let Data = await response.json();
+    return Data;
+  };
+let Data = await getData();
+console.log(Data['Product']);
 
 type ShoppingCartTableProps = {
     changable: boolean;
@@ -14,12 +18,12 @@ type ShoppingCartTableProps = {
 };
 
 type CartItemType = {
-    id: number;
-    type: string;
+    product_id: number;
+    product_type: string;
     name: string;
     quantity: number;
     price: number;
-};
+}
 
 const ShoppingCartTable = ({
     changable,
@@ -27,84 +31,68 @@ const ShoppingCartTable = ({
     setCartItems,
     tableTitle
 }: ShoppingCartTableProps) => {
+    if(Data['Total_Price'] === 0){
+        return(
+            <>
+            <div style={{fontSize:"20px"}}>You don't have anything to checkout</div>
+            </>
+        )
+    }
+    //const [cartItems, setCartItems] = React.useState<CartItemType[]>(Data['Product']);
 
-    const getImg = (type: string) => {
-        return type === 'ram' ? ramImg : type === 'hdd' ? hddImg : ssdImg;
-    };
+    const getImg = (product_type: string) => {
+        return product_type === 'RAM' ? "http://localhost:5173/static/src/assets/images/ram.png" : product_type === 'HHD' ? "http://localhost:5173/static/src/assets/images/hdd.png" : "http://localhost:5173/static/src/assets/images/ssd.png";
+    }
 
     const handleDelete = (id: number) => {
         //For django, we need to send a request to the server to delete the item from the database
-        const newCartItems = cartItems.filter((cartItem) => cartItem.id !== id);
+        const newCartItems = cartItems.filter((cartItem) => cartItem.product_id !== id);
         setCartItems(newCartItems);
     };
 
     const calculateTotal = () => {
-        //For django, we need to send a request to the server to calculate the total price
         return cartItems.reduce((total, item) => total + item.price * item.quantity, 0).toLocaleString('zh-TW', { style: 'currency', currency: 'TWD' });
-    };
+    }
 
     return (
         <>
             <table className='shoppingCartTable'>
-                <caption>{tableTitle}</caption>
                 <thead>
                     <tr>
                         <th>View</th>
                         <th>Product</th>
                         <th>Price</th>
                         <th>Quantity</th>
-                        {changable && <th>Action</th>}
                         <th>Total</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {cartItems.map((item) => (
-                        <tr key={item.id}>
-                            <td><img className='shoppingCartTable__img' src={getImg(item.type)} alt="product-view" /></td>
+                {cartItems.map((item) => (
+                        <tr key={item.product_id}>
+                            <td><img className='shoppingCartTable__img' src={getImg(item.product_type)} alt="product-view" /></td>
                             <td>{item.name}</td>
                             <td>{item.price.toLocaleString('zh-TW', { style: 'currency', currency: 'TWD' })}</td>
-                            <td>
-                                <select
-                                    className='shoppingCartTable__quantity'
-                                    value={item.quantity}
-                                    onChange={(e) => {
-                                        const newCartItems = cartItems.map((cartItem) =>
-                                            cartItem.id === item.id
-                                                ? { ...cartItem, quantity: Number(e.target.value) }
-                                                : cartItem
-                                        );
-                                        setCartItems(newCartItems);
-                                    }}
-                                    disabled={!changable}
-                                >
-                                    {Array.from({ length: 10 }, (_, i) => i + 1).map((value) => (
-                                        <option key={value} value={value}>
-                                            {value}
-                                        </option>
-                                    ))}
-                                </select>
-                            </td>
-                            {changable &&
-                                <td>
-                                    <MdDeleteForever className='shoppingCartTable__removeItem' onClick={() => handleDelete(item.id)} />
-                                </td>
-                            }
+                            <td>{item.quantity}</td>
                             <td>{(item.price * item.quantity).toLocaleString('zh-TW', { style: 'currency', currency: 'TWD' })}</td>
                         </tr>
                     ))}
                 </tbody>
                 <tfoot>
                     <tr>
-                        <td colSpan={changable ? 5 : 4} style={{ textAlign: 'right', fontWeight: 'bold' }}>Total</td>
+                        <td style={{ textAlign: 'right', fontWeight: 'bold' }}>Total</td>
                         <td>{calculateTotal()}</td>
                     </tr>
                 </tfoot>
             </table>
             <div className='checkoutButtonContainer'>
-                {changable && <button className='checkoutButton'>Check Out</button>}
+                <form action="../api/removePurchaseList/" role="form" method="post">
+                    <button className='checkoutButton' name="confirm" value="1">Check Out</button>
+                    <button className='checkoutButton'name="confirm" value="0">Cancel</button>
+                </form>
             </div>
         </>
     );
 };
 
 export default ShoppingCartTable;
+
