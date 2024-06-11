3f8c5c1433dd634f813397ea5fceaa3512526a85
Andrea Xu <108392545+ABCDEFG123ddre@users.noreply.github.com>
2024-06-12 01:29:11 +0800

    Update Home.tsx

--- databaseFinalProject/frontend/src/pages/Home/Home.tsx
+++ databaseFinalProject/frontend/src/pages/Home/Home.tsx
@@ -4,31 +4,62 @@ import ShoppingCartTable from '../../components/ShoppingCartTable/ShoppingCartTa
 import './Home.scss'
 import UserProfileCard from '../../components/UserProfileCard/UserProfileCard'
 
-
 type CartItemType = {
-    id: number;
-    type: string;
-    name: string;
-    quantity: number;
-    price: number;
-};
+    order_id: number;
+    Product: string;
+    Order_Time: string;
+    Total_Price: number;
+}
 
+
+const getData = async () => {
+    let response = await fetch("http://127.0.0.1:8000/api/getOrderHistory/");
+    let Data = await response.json();
+    return Data;
+  };
+let Data = await getData();
 
 export default function Home() {
-    const [cartItems, setCartItems] = React.useState<CartItemType[]>([
-        { id: 1, type: "hdd", name: 'Item 1', quantity: 1, price: 10 },
-        { id: 2, type: "ssd", name: 'Item 2', quantity: 2, price: 20 },
-        { id: 3, type: "ssd", name: 'Item 3', quantity: 3, price: 30 },
-    ]);
+    if (Data[0]['Total_Price'] === 0){
+        return(
+            <div className='homePage'>
+            <UserProfileCard></UserProfileCard>
+        </div>
+        )
+    }
+    const [cartItems, setCartItems] = React.useState<CartItemType[]>(Data);
+    const renderList = (item) => {
+        const listItems = [];
+        let len = item.length;
+        for (let i = 0; i<len; i++) {
+          listItems.push(
+            <li>{item[i]['name']} ({item[i]['quantity']})</li>
+        );}
+        return listItems;
+      };
     return (
         <div className='homePage'>
             <UserProfileCard></UserProfileCard>
-            <ShoppingCartTable
-                changable={false}
-                cartItems={cartItems}
-                setCartItems={setCartItems}
-                tableTitle='History Order'
-            ></ShoppingCartTable>
+            <table className='shoppingCartTable'>
+                <thead>
+                    <tr>
+                        <th>Order History List</th>
+                        <th>Product</th>
+                        <th>Order Time</th>
+                        <th>Total Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                {cartItems.map((item) => (
+                        <tr key={item.order_id}>
+                            <td>{item.order_id}</td>
+                            <td><ul>{renderList(item.Product)}</ul></td>
+                            <td>{item.Order_Time}</td>
+                            <td>{item.Total_Price}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
         </div>
     )
 }
