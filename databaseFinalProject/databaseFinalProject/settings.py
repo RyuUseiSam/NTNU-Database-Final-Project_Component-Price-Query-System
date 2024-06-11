41b93a698292d53a677534ed131f3c658600a32b
Andrea Xu <108392545+ABCDEFG123ddre@users.noreply.github.com>
2024-06-12 01:48:17 +0800

    Update settings.py

--- databaseFinalProject/databaseFinalProject/settings.py
+++ databaseFinalProject/databaseFinalProject/settings.py
@@ -11,10 +11,6 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
-import pymysql
-
-# Install MySQLdb
-pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -42,16 +38,19 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api', # Add the api app to the list of installed apps
+    'rest_framework',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'databaseFinalProject.urls'
@@ -79,13 +78,6 @@ WSGI_APPLICATION = 'databaseFinalProject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -93,10 +85,21 @@ DATABASES = {
         'USER': 'team13',  #資料庫帳號
         'PASSWORD': 'VvCUAh]4FTD!O4D_',  #資料庫密碼
         'HOST': '140.122.184.129',  #Server(伺服器)位址
-        'PORT': '3310'  #PostgreSQL Port號
+        'PORT': '3310',  #PostgreSQL Port號
     }
 }
-
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'DBFinalProject',  #資料庫名稱
+        'USER': 'postgres',  #資料庫帳號
+        'PASSWORD': 'vm3bjo45pA',  #資料庫密碼
+        'HOST': '127.0.0.1',  #Server(伺服器)位址
+        'PORT': '5432'  #PostgreSQL Port號
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -134,15 +137,21 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# Default primary key field type    
+# Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+"""
 CSRF_COOKIE_SAMPSITE = 'Strict'
 SESSION_COOKIE_SAMESITE = 'Strict'
 CSRF_COOKIE_HTTPONLY = False
 SESSION_COOKIE_HTTPONLY = True
+"""
+CSRF_COOKIE_SAMESITE = None
+SESSION_COOKIE_SAMESITE = None
+CSRF_COOKIE_HTTPONLY = False
+SESSION_COOKIE_HTTPONLY = False
+APPEND_SLASH=False
 
 STATICFILES_DIRS =(
     BASE_DIR.joinpath('frontend', 'dist'),
