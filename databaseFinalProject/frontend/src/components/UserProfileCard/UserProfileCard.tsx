c82428efe92f12f1235d1b251965178aa0aa9759
Andrea Xu <108392545+ABCDEFG123ddre@users.noreply.github.com>
2024-06-12 01:33:46 +0800

    Update UserProfileCard.tsx

--- databaseFinalProject/frontend/src/components/UserProfileCard/UserProfileCard.tsx
+++ databaseFinalProject/frontend/src/components/UserProfileCard/UserProfileCard.tsx
@@ -4,30 +4,27 @@ import './UserProfileCard.scss'
 
 import { FaPenAlt } from "react-icons/fa";
 
-
-import profileIcon from '../../assets/images/profileIcon.png';
-
+const getData = async () => {
+  let response = await fetch("http://127.0.0.1:8000/api/get_username/");
+  let Data = await response.json();
+  return Data;
+};
+let Data = await getData();
 
 const UserProfileCard = () => {
-  const [userNames, setUserNames] = useState('User01')
+  const [userNames, setUserNames] = useState(Data["username"])
 
 
   return (
     <div className='userProfileCard'>
       <div className="userProfileCard__gradient"></div>
       <div className="userProfileCard__profile-down">
-        <img src={profileIcon} alt="" />
+        <img src={"http://localhost:5173/static/src/assets/images/profileIcon.png"} alt="" />
         <div className="userProfileCard__title">{userNames}</div>
-        <div className="userProfileCard__description">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure at illum quam aperiam architecto a in, quo, dignissimos animi, perspiciatis vero molestiae officia inventore. Amet ea deleniti nesciunt obcaecati sapiente!
-        </div>
+        <form action="../api/deleteAccount/" role="form" method="post">
+          <button style={{color: "red", border: "none", background: "none"}}>Delete Account</button>
+        </form>
       </div>
-      <div className="userProfileCard__button">
-        <a><FaPenAlt></FaPenAlt></a>
-      </div>
-
-
-
     </div>
   )
 }
