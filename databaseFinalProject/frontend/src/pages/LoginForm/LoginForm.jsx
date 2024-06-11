7c3275035213a0bfc73cee2a10862d997351988b
Andrea Xu <108392545+ABCDEFG123ddre@users.noreply.github.com>
2024-06-12 01:29:37 +0800

    Update LoginForm.jsx

--- databaseFinalProject/frontend/src/pages/LoginForm/LoginForm.jsx
+++ databaseFinalProject/frontend/src/pages/LoginForm/LoginForm.jsx
@@ -4,25 +4,16 @@ import { FaUser, FaLock } from "react-icons/fa";
 
 // Form Component
 const Form = ({ isRegister, handleLogin, toggleForm }) => (
-  <form onSubmit={handleLogin}>
+  <form action={isRegister ? "/api/submitAccount/" : "/api/login/"} role="form" method="post">
     <h1>{isRegister ? "Register" : "Login"}</h1>
     <div className="login-wrapper__input-box">
-      <input className="login-wrapper__user-inputbox" type="text" placeholder="Username" />
+      <input className="login-wrapper__user-inputbox" type="text" placeholder="Username" name="username"/>
       <FaUser className="login-wrapper__icon" />
     </div>
     <div className="login-wrapper__input-box">
-      <input className="login-wrapper__password-inputbox" type="password" placeholder="Password" />
+      <input className="login-wrapper__password-inputbox" type="password" placeholder="Password" name="password"/>
       <FaLock className="login-wrapper__icon" />
     </div>
-    {!isRegister && (
-      <div className="login-wrapper__remember-forget">
-        <label>
-          <input type="checkbox" />
-          Remember me
-        </label>
-        <a href="#">Forget password?</a>
-      </div>
-    )}
     <button className="login-wrapper__submit">{isRegister ? "Register" : "Login"}</button>
     <div className="login-wrapper__register-link">
       <p>
@@ -38,9 +29,7 @@ const Form = ({ isRegister, handleLogin, toggleForm }) => (
 // Logged In Component
 const LoggedIn = ({ username, handleLogout }) => (
   <div className="logged-in-message">
-    <h1>
-      Already Logged In !
-    </h1>
+    <h1>Already Logged In!</h1>
     <div className="login-wrapper__input-box">
       <input className="login-wrapper__user_info" type="text" placeholder="Username" value={username} disabled />
       <FaUser className="login-wrapper__icon" />
@@ -60,12 +49,6 @@ const LoginForm = () => {
     setIsRegister((prev) => !prev);
   };
 
-  const handleLogin = (event) => {
-    event.preventDefault();
-    setIsLoggedIn(true);
-    setUsername("User123");
-  };
-
   const handleLogout = () => {
     setIsLoggedIn(false);
   };
@@ -74,7 +57,7 @@ const LoginForm = () => {
     <div className="login-page">
       <div className="login-wrapper">
         {!isLoggedIn ? (
-          <Form isRegister={isRegister} handleLogin={handleLogin} toggleForm={toggleForm} />
+          <Form isRegister={isRegister} toggleForm={toggleForm} />
         ) : (
           <LoggedIn username={username} handleLogout={handleLogout} />
         )}
