e893693427ecfe5006b3941c6bd9060eeef32d9e
Andrea Xu <108392545+ABCDEFG123ddre@users.noreply.github.com>
2024-06-12 01:32:12 +0800

    Update CartItems.tsx

--- databaseFinalProject/frontend/src/components/CartItems/CartItems.tsx
+++ databaseFinalProject/frontend/src/components/CartItems/CartItems.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import { MdDeleteForever } from "react-icons/md";
 
-import ramImg from '../../assets/images/ram.png';
-import hddImg from '../../assets/images/hdd.png';
-import ssdImg from '../../assets/images/ssd.png';
+//import ramImg from "http://localhost:5173/static/src/assets/images/ram.png";
+//import hddImg from "http://localhost:5173/static/src/assets/images/hdd.png";
+//import ssdImg from "http://localhost:5173/static/src/assets/images/ssd.png";
 
 import './CartItems.scss'
 
 
-const CartItems = ({ id, type, name, quantity, price }) => {
+const CartItems = ({ product_id, product_type, name, quantity, price, left }) => {
     const [cartQuantity, setCartQuantity] = React.useState(quantity);
-    const img = type === 'ram' ? ramImg : type === 'hdd' ? hddImg : ssdImg;
+    const img = product_type === 'RAM' ? "http://localhost:5173/static/src/assets/images/ram.png" : product_type === 'HHD' ? "http://localhost:5173/static/src/assets/images/hdd.png" : "http://localhost:5173/static/src/assets/images/ssd.png";
     const formattedPrice = price.toLocaleString('zh-TW', { style: 'currency', currency: 'TWD' });
 
     return (
@@ -21,21 +21,26 @@ const CartItems = ({ id, type, name, quantity, price }) => {
             </div>
             <div>{formattedPrice}</div>
 
-
-            <select className='CartItem__quantity' value={cartQuantity} onChange={(e) => setCartQuantity(e.target.value)}>
-                {Array.from({ length: 10 }, (_, i) => i + 1).map((value) => (
+            <select className='CartItem__quantity' value={cartQuantity} onChange={
+                (e) => {
+                    const newQuantity = Number(e.target.value)
+                    setCartQuantity(newQuantity)}
+                }>
+                {Array.from({ length: left }, (_, i) => i + 1).map((value) => (
                     <option key={value} value={value}>
                         {value}
                     </option>
                 ))}
             </select>
+            <input type="text" name={"quantity" + product_id} style={{display: 'none'}} value={cartQuantity}/>
 
-            <div>{(price * quantity).toLocaleString('zh-TW', { style: 'currency', currency: 'TWD' })}</div>
-            <div className='CartItem__remove-btn'>
-                <MdDeleteForever />
-            </div>
+            <form action="../api/remove_from_cart/" role="form" method="post">
+                <input type="text" name="ProductID" style={{display: 'none'}} value={product_id}/>
+                <button className='CartItem__remove-btn' style={{border: "none", background: "none"}}><MdDeleteForever /></button>
+            </form>
         </div>
     )
 }
 
 export default CartItems
+//<div>{(price * quantity).toLocaleString('zh-TW', { style: 'currency', currency: 'TWD' })}</div>
