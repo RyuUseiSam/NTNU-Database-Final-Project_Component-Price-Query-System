ef7163172ef5e90b69f60a660bff7260efd884c9
Andrea Xu <108392545+ABCDEFG123ddre@users.noreply.github.com>
2024-06-12 01:28:11 +0800

    Create App_login.jsx

---
+++ databaseFinalProject/frontend/src/App_login.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Login
+import LoginForm from "./pages/LoginForm/LoginForm";
+
+
+function App() {
+  return (
+    <>
+      <LoginForm></LoginForm>
+    </>
+    
+  );
+}
+
+export default App;
